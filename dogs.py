5b827867736b42d40d4a6939ebcf7a3da8941585
Stevenlutton <steven.lutton2128@gmail.com>
2019-05-07 17:14:28 -0600

    first question works

--- dogs.py
+++ dogs.py
@@ -1,5 +1,11 @@
 info = "I love dogs so I would like to start a project on them."
 print(info)
-
+print(" ")
 favorite = input("Who is your favorite dog?  ")
 print(favorite, "is an amazing dog!")
+if favorite == "Jolie" :
+    print(favorite, "is also my favorite dog!")
+elif favorite == "Nelly" :
+    print(favorite, "is my sister's favorite dog!")
+else : 
+    print("I can see why", favorite, "is your favorite")
