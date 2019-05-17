f4d6dd9e7efa9b7b06ac30df19df8fb74b8169e4
Stevenlutton <steven.lutton2128@gmail.com>
2019-05-17 09:34:27 -0600

    python practice

--- check.py
+++ check.py
@@ -1,8 +1,16 @@
 import math
 
 def split_check(total, number_of_people):
-    cost_per_person = math.ceil(total / number_of_people)
-    return cost_per_person
+    if number_of_people <= 1:
+        raise ValueError("More than 1 person required to split check")
+    return math.ceil(total / number_of_people)
 
-amount_due = split_check(84.97, 4)
-print("Each person owes ${}".format(amount_due))
+try:
+    total_due =float(input("What is the total?    "))
+    number_of_people = int(input("How many people?    "))
+    amount_due = split_check(total_due, number_of_people)
+except ValueError as err:
+    print("Oh no! That's not a valid value. Try again...")
+    print("({})".format(err))
+else:
+    print("Each person owes ${}".format(amount_due))
