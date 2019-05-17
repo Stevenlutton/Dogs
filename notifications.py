36de4c3c4c740ed256698c35d0b7182efe3988c1
Stevenlutton <steven.lutton2128@gmail.com>
2019-05-17 09:48:06 -0600

    moved files

--- notifications.py
+++
@@ -1,9 +0,0 @@
-def yell(text):
-    text = text.upper()
-    number_of_characters = len(text)
-    result = text + "!" * (number_of_characters // 4)
-    print(result)
-
-yell("you are doing great")
-yell("Don't forget to ask for help")
-yell("Don't Repeat Yourself. Keep things DRY")
